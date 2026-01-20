ad75eaba04ac186fd4f8b6ec5b3e553fdac312b6
KeithPatarroyo <keithypts@gmail.com>
2026-01-20 23:21:15 +0000

    added forward pass with new data

--- python/forward_pass.py
+++ python/forward_pass.py
@@ -12,26 +12,39 @@ def get_neighboors(grid_x,grid_y):
 
     return middle + r_one + corners + r_two
 
-def get_string(start, grid_x,grid_y):
+def get_info_position(start, grid_x,grid_y):
 
-    with open('test_data/tokens_epoch_{}.csv'.format(start), 'r') as f:
+    with open('test_data/pairings_epoch_{}.csv'.format(start), 'r',encoding='utf-8', errors='ignore') as f:
         content = f.read()
-        fixed_content = content.replace(',"""\n', ',""""\n')
 
     # Load into pandas from the fixed string
-    df = pd.read_csv(io.StringIO(fixed_content))
+    df = pd.read_csv(io.StringIO(content))
 
-    filtered = df[df["grid_x"]==grid_x]
-    chars_n = filtered[filtered["grid_y"]==grid_y]["char"].to_list()
+    filtered = df[df["position_x"]==grid_x]
+    filtered_df = filtered[filtered["position_y"]==grid_y]
+
+    return filtered_df
+
+def get_string_pairing(start, grid_x,grid_y):
+
+    filtered = get_info_position(start, grid_x,grid_y)
+    string = filtered["program"].tolist()[0]
 
     instructions = [",",".","[","]","{","}","<",">","+","-"]
     chars = []
-    for i in chars_n:
-        if chr(i) in instructions:
-            chars.append(chr(i))
+    for i in string:
+        if i in instructions:
+            chars.append(i)
         else:
             chars.append(" ")
     return "".join(chars)
+
+def get_prev_pos(start, grid_x,grid_y):
+
+    filtered = get_info_position(start, grid_x,grid_y)
+    comb_x = filtered["combined_x"].tolist()[0]
+    comb_y = filtered["combined_y"].tolist()[0]
+    return comb_x,comb_y
 
 def check_replicator(string):
 
@@ -46,23 +59,54 @@ def check_replicator(string):
     return is_repeated
 
 def find_replicators(start,grid_x,grid_y,last):
+
     sf_programs = {}
 
-    sf_programs[start] = [(grid_x,grid_y,get_string(start,grid_x,grid_y))]
+    sf_programs[start] = [(grid_x,grid_y,get_string_pairing(start,grid_x,grid_y))]
 
     for time in range(start,last):
         sf_programs[time+1] = []
         for replicators in sf_programs[time]:
+
+            pair_neighboor = ()
             
             for neigh_x, neigh_y in get_neighboors(replicators[0],replicators[1]):
 
-                next = get_string(time+1,neigh_x,neigh_y)
-                dif = [ next[i] == replicators[2][i] for i in range(len(next)) ]
-                mod_rate = sum(dif)/len(dif)
+                pair_neighboor = get_prev_pos(time+1, neigh_x, neigh_y)
+                
 
-                if mod_rate > 0.9:
-                    if check_replicator(next):
-                        sf_programs[time+1].append((neigh_x,neigh_y,next))
+                if (replicators[0],replicators[1]) == pair_neighboor:
+                    
+                    next = get_string_pairing(time+1,neigh_x,neigh_y)
+                    dif = [ next[i] == replicators[2][i] for i in range(len(next)) ]
+                    mod_rate = sum(dif)/len(dif)
+
+                    #print(next)
+
+                    if mod_rate > 0.9:
+                        if check_replicator(next):
+                            sf_programs[time+1].append((neigh_x,neigh_y,next))
+
+                    next_same = get_string_pairing(time+1,replicators[0],replicators[1])
+                    dif = [ next_same[i] == replicators[2][i] for i in range(len(next)) ]
+                    mod_rate = sum(dif)/len(dif)
+
+                    #print(next_same)
+
+                    if mod_rate > 0.9:
+                        if check_replicator(next_same):
+                            sf_programs[time+1].append((replicators[0],replicators[1],next_same))
+
+                
+                if  pair_neighboor == (-1,-1) and (neigh_x, neigh_y) ==(replicators[0],replicators[1]):
+
+                    next = get_string_pairing(time+1,neigh_x,neigh_y)
+                    dif = [ next[i] == replicators[2][i] for i in range(len(next)) ]
+                    mod_rate = sum(dif)/len(dif)
+
+                    if mod_rate > 0.9:
+                        if check_replicator(next):
+                            sf_programs[time+1].append((neigh_x,neigh_y,next))
                 
         print("found {} replicators in t={}".format(len(sf_programs[time+1]),time+1))
 
